e7d60b1b0b1fbe50bd60644f90c4ff913e32f3c8
Sara Elmenshawy <saraelmenshawy273@gmail.com>
2017-07-29 20:28:19 +0200

    Event Binding
    Create a button
    On click event show image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,24 @@ import { Component } from '@angular/core';
     <li *ngFor="let arr of monEtre">{{ arr }}
     </li>
   </ul>
+
+  <button (click)="myEvent($event)">Display Verb</button>
+  <div [hidden]= "edit">
   <img [src] = "angularLogo"/>
+  </div>
 `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   // title = 'app';
-angularLogo= 'http://1.bp.blogspot.com/-Zi023Bis7Es/UnBiSAHuWZI/AAAAAAAAAHE/lxcmIQjP5e8/s1600/etre2.png';
+  angularLogo= 'http://1.bp.blogspot.com/-Zi023Bis7Es/UnBiSAHuWZI/AAAAAAAAAHE/lxcmIQjP5e8/s1600/etre2.png';
   monEtre = ['Je suis', 'Tu es', 'Elle/Il est', 'Nous somme', 'Vous etes', 'Elles/Ils sont'];
   myObject= {
   Etre: 'Etre [To Be]',
   Avoir : 'Avoir [To Have]'
   };
+  edit= true;
+  myEvent(event) {
+   this.edit = false;
+  }
 }
